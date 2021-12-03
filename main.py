4dad9542b856c26a826ab2c24d584837460b8a7e
Xiaoyang Hu <huxiaoyanghfut@gmail.com>
2021-12-03 08:49:21 +0800

    第一个版本

--- main.py
+++ main.py
@@ -5,56 +5,69 @@ import matplotlib as mpl
 import copy
 from initialization import initialization
 from cal_objective import cal_objective
-from neighboringX import neighboringX
-from parse_data import parse_data
+from neighboring_cur import neighboring_cur
+import time
+from parse_data_new1 import parse_data_new1
+from parse_data_new2 import parse_data_new2
+a = time.time()
 def main():
-    job_machine, processing_time, machine_number, operation_number = parse_data()
+    job_machine, processing_time, machine_number, operation_number = parse_data_new1()
+    # job_machine, processing_time, machine_number, operation_number = parse_data_new2()
+
     # 设置参数
-    H = 50
+    H = 5
     MAX_ITERATION = 1000000
-    MAX_NO_IMPROVE = 0.03
+    MAX_NO_IMPROVE = 0.02
     # 初始化当前解X，并计算其目标函数值makespan f(x)
-    X = initialization(operation_number, job_machine)
-    bestX = X
-    objective_value_X = cal_objective(X, machine_number, operation_number, job_machine, processing_time)
-    best_Cmax_time = objective_value_X
-    history_list = [objective_value_X for i in range(H)]
+    cur = initialization(operation_number, job_machine)
+    bestX = cur
+    objective_value_cur = cal_objective(cur, machine_number, operation_number, job_machine, processing_time)
+    best_Cmax_time = objective_value_cur
+    history_list = [objective_value_cur for i in range(H)]
     it = 0
     idle = 0
     trace_min = []
     trace_Y = []
     #循环过程
     while (it < MAX_ITERATION) and (idle < MAX_NO_IMPROVE*MAX_ITERATION):
-        Y = neighboringX(X, job_machine, operation_number)
-        #TODO 多邻域选择
-        objective_value_Y = cal_objective(Y, machine_number, operation_number, job_machine, processing_time)
-        if objective_value_Y >= objective_value_X:
+        next = neighboring_cur(cur, job_machine, operation_number)
+        #TODO 多邻域选择 
+        objective_value_next = cal_objective(next, machine_number, operation_number, job_machine, processing_time)
+        if objective_value_next >= objective_value_cur:
             idle += 1
         else:
             idle = 0
-        if objective_value_Y < best_Cmax_time:
-            bestX = Y
-            best_Cmax_time = objective_value_Y
-            print('最优解的完工时间是：%d\n', best_Cmax_time)
+        if objective_value_next < best_Cmax_time:
+            bestX = next
+            best_Cmax_time = objective_value_next
+            print('最优解的完工时间是：%f' % best_Cmax_time)
         h = it % H
-        if objective_value_Y < history_list[h] or objective_value_Y <= objective_value_X:
-            X = Y
-            objective_value_X = objective_value_Y
-        if objective_value_Y < history_list[h]:
-            history_list[h] = objective_value_Y
+        if objective_value_next < history_list[h] or objective_value_next <= objective_value_cur:
+            cur = copy.deepcopy(next)
+            objective_value_cur = objective_value_next
+        if objective_value_next < history_list[h]:
+            history_list[h] = objective_value_next
         it += 1
         trace_min.append(min(history_list))
-        trace_Y.append(objective_value_Y)
-        print("最优解的完工时间：%f\n", best_Cmax_time)
-        print("最优解是：\n")
-        print(bestX)
+        trace_Y.append(objective_value_next)
+    b = time.time()
+    seconds = b - a
+    print("程序用时：%02d s" % seconds)
+    print("最优解的完工时间：%f" %best_Cmax_time)
+    print("最优解是:")
+    print(bestX)
+    
+    mpl.rcParams['font.sans-serif'] = ['SimHei']
+    mpl.rcParams['axes.unicode_minus'] = False
 
-        mpl.rcParams['font.sans-serif'] = ['SimHei']
-        mpl.rcParams['axes.unicode_minus'] = False
-        plt.plot(trace_min)
-        plt.plot(trace_Y)
-        plt.title("算法曲线收敛图")
-        plt.xlabel("迭代次数")
-        plt.ylabel("最大完工时间")
-if __name__ == '_main_':
+    # fig = plt.figure()
+    x_axis = [i for i in range(it)]
+    plt.plot(x_axis, trace_min, label='历史最优解')
+    # plt.plot(x_axis, trace_Y, label='候选解')
+    plt.title("算法曲线收敛图")
+    plt.xlabel("迭代次数")
+    plt.ylabel("最大完工时间")
+    plt.legend()
+    plt.show()
+if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-#1.解析数据得到JobMachine、ProcesscingTime、MachineNumber、OperationNumber
+# 1.解析数据得到JobMachine、ProcesscingTime、MachineNumber、OperationNumber
 from types import ClassMethodDescriptorType
 import pandas as pd
 import numpy as np
-
+import math
+from cal_objective import cal_objective
+from initialization import initialization
+from main import main
+from neighboring_cur import neighboring_cur
 from parse_data import parse_data
+from parse_data_new1 import parse_data_new1
+
 # job_machine_tmp = pd.read_excel("排产输入数据样例.xlsx",sheet_name="工序可选机器表")
 # processing_time_tmp = pd.read_excel("排产输入数据样例.xlsx",sheet_name="各工序加工时间")
 # print(job_machine_tmp)
@@ -20,13 +26,61 @@ from parse_data import parse_data
 # str_tmp = "2,4"
 
 # print(list(map(int, str_tmp.split(','))))
-job_machine, processing_time, machine_number, operation_number = parse_data()
-print('\n')
-print(job_machine)
-print('\n')
-print(processing_time)
-print('\n')
-print(machine_number)
-print('\n')
-print(operation_number)
-print('\n')
+
+# 数据解析测试 OK
+# job_machine, processing_time, machine_number, operation_number = parse_data()
+# job_machine, processing_time, machine_number, operation_number = parse_data_new1()
+
+# print('工序机器表：')
+# print(job_machine)
+
+# print('工序时间表：')
+# print(processing_time)
+
+# print('机器数量：')
+# print(machine_number)
+
+# print('各工件工序数量：')
+# print(operation_number)
+
+# 测试初始化结果 OK
+# job_machine, processing_time, machine_number, operation_number = parse_data()
+# job_machine, processing_time, machine_number, operation_number = parse_data_new1()
+# cur = initialization(operation_number, job_machine)
+# print(sum(operation_number))
+# print(len(cur))
+# print(cur)
+
+# 测试计算目标函数值的计算调度工序部分 OK
+# print("测试求目标函数")
+# job_machine, processing_time, machine_number, operation_number = parse_data()
+# job_machine, processing_time, machine_number, operation_number = parse_data_new1()
+# cur = initialization(operation_number, job_machine)
+# objective_value = cal_objective(cur, machine_number, operation_number, job_machine, processing_time)
+# print(objective_value)
+
+# 测试计算目标函数值的计算工序时间（每个工序的开工时间和完工时间）和最大完工时间 OK
+# print("测试求目标函数")
+# job_machine, processing_time, machine_number, operation_number = parse_data()
+# cur = initialization(operation_number, job_machine)
+# objective_value = cal_objective(cur, machine_number, operation_number, job_machine, processing_time)
+# print(objective_value)
+
+# 测试生成cur的邻域解 OK
+# print("测试生成cur的邻域解")
+# job_machine, processing_time, machine_number, operation_number = parse_data()
+# job_machine, processing_time, machine_number, operation_number = parse_data_new1()
+# cur = initialization(operation_number, job_machine)
+# print("我是分割线=======================")
+# print(cur)
+# print("我是分割线=======================")
+# next, index1, index2, index3 = neighboring_cur(cur, job_machine, operation_number)
+# print(next)
+# print("我是分割线=======================")
+# total_operations = math.floor(len(cur)/2)
+# print([cur[total_operations:len(cur)][index1],next[total_operations:len(next)][index1]])
+# print([cur[index2],next[index2]])
+# print([cur[index3],next[index3]])
+
+# 测试主函数main OK
+# main()
